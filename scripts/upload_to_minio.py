cc2f24e51b80255f682aa42a1b4d7fee30a245b9
pernebay-arailym <pernebayarailym@gmail.com>
2026-06-04 10:54:58 +0200

    feat: use environment variables for AWS credentials in upload script

--- scripts/upload_to_minio.py
+++ scripts/upload_to_minio.py
@@ -1,10 +1,11 @@
 import boto3
+import os
 
 s3 = boto3.client(
     "s3",
     endpoint_url="http://localhost:9000",
-    aws_access_key_id="minioadmin",
-    aws_secret_access_key="minioadmin"
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
 )
 
 files = [
